fa345516c00126c4f031012941859da26954135e
swapnil <sangavkarswapnil2172@gmail.com>
2025-03-06 22:58:13 +0530

    installed jar lombok and add lombok jar in sts.exe path

--- src/main/java/com/reports/entity/CitizenPlan.java
+++ src/main/java/com/reports/entity/CitizenPlan.java
@@ -3,8 +3,10 @@ package com.reports.entity;
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="CITIZEN_PlAN_INFO")
 public class CitizenPlan {
 	@Id
@@ -20,74 +22,6 @@ public class CitizenPlan {
 	private String rejectionRsn;
 	private LocalDate terminateDate;
 	private String terminateRsn;
-	public Integer getCitizenId() {
-		return citizenId;
-	}
-	public void setCitizenId(Integer citizenId) {
-		this.citizenId = citizenId;
-	}
-	public String getCitizenName() {
-		return citizenName;
-	}
-	public void setCitizenName(String citizenName) {
-		this.citizenName = citizenName;
-	}
-	public String getPlanName() {
-		return planName;
-	}
-	public void setPlanName(String planName) {
-		this.planName = planName;
-	}
-	public String getPlanStatus() {
-		return planStatus;
-	}
-	public void setPlanStatus(String planStatus) {
-		this.planStatus = planStatus;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public LocalDate getPlanStartDate() {
-		return planStartDate;
-	}
-	public void setPlanStartDate(LocalDate planStartDate) {
-		this.planStartDate = planStartDate;
-	}
-	public LocalDate getPlanEndDate() {
-		return planEndDate;
-	}
-	public void setPlanEndDate(LocalDate planEndDate) {
-		this.planEndDate = planEndDate;
-	}
-	public Double getBenfitAmt() {
-		return benfitAmt;
-	}
-	public void setBenfitAmt(Double benfitAmt) {
-		this.benfitAmt = benfitAmt;
-	}
-	public String getRejectionRsn() {
-		return rejectionRsn;
-	}
-	public void setRejectionRsn(String rejectionRsn) {
-		this.rejectionRsn = rejectionRsn;
-	}
-	public LocalDate getTerminateDate() {
-		return terminateDate;
-	}
-	public void setTerminateDate(LocalDate terminateDate) {
-		this.terminateDate = terminateDate;
-	}
-	public String getTerminateRsn() {
-		return terminateRsn;
-	}
-	public void setTerminateRsn(String terminateRsn) {
-		this.terminateRsn = terminateRsn;
-	}
-	
-	
 
 
 }
--- src/main/java/com/reports/runner/DataLoader.java
+++ src/main/java/com/reports/runner/DataLoader.java
@@ -229,6 +229,7 @@ public class DataLoader implements ApplicationRunner {
 		c24.setBenfitAmt(16000.0);
 		
 		
+		
 		List<CitizenPlan> list = Arrays.asList(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24);
 		repo.saveAll(list);
 	}
